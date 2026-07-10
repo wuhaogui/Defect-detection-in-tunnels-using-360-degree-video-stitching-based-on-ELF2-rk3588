ee99e21e53d91bf74eed4eb287ef7050377b19a9
wuhaogui <3332841507@qq.com>
2026-07-10 18:49:43 +0800

    Delete stiii/stiii directory

--- stiii/stiii/worker.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-#include <atomic>
-#include "frame_queue.h"
-#include "frame_stit_queue.h"
-
-void worker(framequeue* frameQ, stitqueue* stitQ, std::atomic<bool>* exitFlag);
